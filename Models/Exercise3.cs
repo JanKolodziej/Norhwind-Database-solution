758d978f033fd97a9730b0f0877980e1a744c94b
JanKolodziej <jankolodziej@outlook.com>
2025-12-12 15:14:21 +0100

    Finished exercise 3 Exercise SQL Queries for HAVING

---
+++ Models/Exercise3.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NW_Class_lib
+{
+    public class Exercise3 //Exercise SQL Queries for HAVING
+    {
+        private NorthwindContext db;
+        public Exercise3(NorthwindContext northwindContext)
+        {
+            db = northwindContext;
+        }
+        public int Report_Delayed_shipments() //Create a report that shows the order ids and the associated employee names for orders that shipped after the required date (37 rows)
+        {
+            var report = db.Orders.Where(o => o.ShippedDate.Value > o.RequiredDate).Select(o => new
+            {
+                o.OrderId,
+                o.Employee
+            }).ToList();
+            return report.Count;
+        }
+        public int Report_Quantity_Of_Products() //Create a report that shows the total quantity of products (from the Order_Details table) ordered.
+                                                 //Only show records for products for which the quantity ordered is fewer than 200 (5 rows)
+        {
+            var report = db.OrderDetails.Include(o=> o.Product).GroupBy(o => o.ProductId).Select(o => new
+            {
+                Id=o.Key,
+                Number=o.Sum(g=>g.Quantity)
+            }).Where(o => o.Number < 200).ToList();
+            return report.Count;
+        }
+        //Create a report that shows the total number of orders by Customer since December 31,1996.
+        //The report should only return rows for which the total number of orders is greater than 15 (5 rows)
+        public int Report_Total_number_orders()
+        {
+            var report = db.Orders.Include(c => c.Customer).GroupBy(o => o.CustomerId).Select(o => new
+            {
+                ID = o.Key,
+                NumberOfOrders = o.Count()
+            }).Where(o => o.NumberOfOrders > 15).ToList();
+            return report.Count;
+        }
+    }
+}
---
+++ NW_Tests/UnitTest3.cs
@@ -0,0 +1,42 @@
+﻿using Models.Models;
+using NW_Class_lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NW_Tests
+{
+    public class UnitTest3
+    {
+        private readonly NorthwindContext _context = new();
+        [Fact]
+        public void Test_Report_Delayed_shipments()
+        {
+
+            Exercise3 ex = new(_context);
+            var wynik = ex.Report_Delayed_shipments();
+            Assert.Equal(37, wynik);
+
+        }
+        [Fact]
+        public void Test_Report_Quantity_Of_Products()
+        {
+
+            Exercise3 ex = new(_context);
+            var wynik = ex.Report_Quantity_Of_Products();
+            Assert.Equal(5, wynik);
+
+        }
+        [Fact]
+        public void Test_Report_Total_number_orders()
+        {
+
+            Exercise3 ex = new(_context);
+            var wynik = ex.Report_Quantity_Of_Products();
+            Assert.Equal(5, wynik);
+
+        }
+    }
+}
